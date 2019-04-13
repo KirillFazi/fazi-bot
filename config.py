ff1fcf569e2ef0a7b94317a607e94d23f3e328fa
KirillFazi <49456329+KirillFazi@users.noreply.github.com>
2019-04-13 21:24:08 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+token = '587169154:AAHrF-djNcOGOI2NE1Evjq8jtL80dipvUpE'  # Токен бота
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
